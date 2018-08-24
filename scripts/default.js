8c7e3d3f139c974aec073555a4114121af4324eb
Brian Tomlinson <btomlins@redhat.com>
2018-08-24 08:37:29 -0700

    Use short URL for COP mojo page, add Ansible blog as resource

--- scripts/default.js
+++ scripts/default.js
@@ -2,9 +2,10 @@ module.exports = (robot) => {
   robot.respond(/info(?![\s|\w|\d])/, (res) => {
     res.reply(`
       Basic help and resources:
-      *Automation COP Mojo Page:* https://mojo.redhat.com/community/communities-at-red-hat/management/automation-community-of-practice
+      *Automation COP Mojo Page:* https://red.ht/autocop
       *Email list:* ansible-questions@redhat.com
       *Ansible Documentation:* https://docs.ansible.com
+      *Ansible Blog*: https://www.ansible.com/blog
       *Available commands:* help, search, version, maintainer
     `); 
   });
